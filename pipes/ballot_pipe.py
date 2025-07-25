93383bc8e349fbe45d559914b01bfab65b9a7625
Tuomas Poutanen <tuomas.poutanen@gmail.com>
2025-07-25 09:57:33 +0000

    remove sv ballots and votes

--- pipes/ballot_pipe.py
+++ pipes/ballot_pipe.py
@@ -11,16 +11,17 @@ def preprocess_data():
 
     rows = []
     for ballot in ballot_data[1:]:
-        row = {
-            "id": ballot[0],
-            "title": ballot[12],
-            "session_item_title": ballot[21],
-            "start_time": f"{ballot[9]} Europe/Helsinki",
-            "parliament_id": ballot[31],
-            "minutes_url": ballot[30],
-            "results_url": ballot[28]
-        }
-        rows.append(row)
+        if ballot[1] == "1":  # KieliId == 1
+            row = {
+                "id": ballot[0],
+                "title": ballot[12],
+                "session_item_title": ballot[21],
+                "start_time": f"{ballot[9]} Europe/Helsinki",
+                "parliament_id": ballot[31],
+                "minutes_url": ballot[30],
+                "results_url": ballot[28]
+            }
+            rows.append(row)
 
     with open(csv_path, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=rows[0].keys())
