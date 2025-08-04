12850999f1ebffd86d44a94a95fdad53321fbbdd
kettukarkki <matias.ikola@hotmail.com>
2025-08-04 07:20:10 +0000

    feat: :sparkles: Add parser for Vaski filetypes. Save them as tsv.

---
+++ vaski_parser.py
@@ -0,0 +1,43 @@
+import os.path
+import pandas as pd
+import numpy as np
+from lxml import etree
+
+
+def vaski_parser():
+    with open(os.path.join("data", "raw", "VaskiData.tsv"), "r") as f:
+        vaski_data = pd.read_csv(f, sep="\t") #, nrows=10000)
+
+    vaski_dict = {}
+    for index, vaski in vaski_data.iterrows():
+        xml = vaski.XmlData
+        root = etree.fromstring(xml)
+        name = root[0][0].text[13:]
+
+        if name.endswith("_fi"):
+            try: 
+                vaski_dict[name] = pd.concat([vaski_dict[name], vaski.to_frame().T])
+
+            except (KeyError, AttributeError) as e:
+                vaski_dict[name] = vaski.to_frame().T
+                vaski_dict[name].columns = ["Id", "XmlData", "Status", "Created", "Eduskuntatunnus", "AttachmentGroupId", "Imported"]
+            
+        elif name.endswith(("_sv", "_en")):
+            continue
+
+        else:
+            try: 
+                vaski_dict[name] = pd.concat([vaski_dict[name], vaski.to_frame().T])
+
+            except (KeyError, AttributeError) as e:
+                vaski_dict[name] = vaski.to_frame().T
+                vaski_dict[name].columns = ["Id", "XmlData", "Status", "Created", "Eduskuntatunnus", "AttachmentGroupId", "Imported"]
+
+    for datatype in vaski_dict:
+        os.makedirs(os.path.join("data", "raw", "vaski"), exist_ok=True)
+        vaski_dict[datatype].to_csv(os.path.join("data", "raw", "vaski", f"{datatype}.tsv"), sep="\t", header=True, index=False)
+
+if __name__ == "__main__":
+    vaski_parser()
+
+
