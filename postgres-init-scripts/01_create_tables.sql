39ee6d9773194cf8a1b2ad004de22bd6c1f668a3
aaltokarkki <90690962+aaltokarkki@users.noreply.github.com>
2025-10-24 13:36:54 +0300

    Rename columns in election_seasons table

--- postgres-init-scripts/01_create_tables.sql
+++ postgres-init-scripts/01_create_tables.sql
@@ -210,9 +210,8 @@ CREATE TABLE IF NOT EXISTS objection_signatures (
 );
 
 CREATE TABLE IF NOT EXISTS election_seasons (
-    id VARCHAR(11) PRIMARY KEY,
-    start_year DATE,
-    end_year DATE,
+    start_date DATE,
+    end_date DATE,
     PRIMARY KEY(start_year, end_year)
 );
 
