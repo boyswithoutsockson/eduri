e64a6974dbcd3ba2688ff49aa1fe7c5d42d6b244
aaltokarkki <matias.ikola@aalto.fi>
2025-11-28 13:36:25 +0000

    fix: :bug: Add newlines to speeches

--- pipes/speeches_pipe.py
+++ pipes/speeches_pipe.py
@@ -117,7 +117,7 @@ def preprocess_data():
                 for para in paragraphs:
                     text = para.text.strip() if para.text else ""
                     if text:
-                        body_parts.append(text)
+                        body_parts.append(f"{text}\n\n")
 
                 # Append puhemies interventions (separately)
                 interventions = speech.xpath(".//vsk:PuheenjohtajaRepliikki", namespaces=ns)
