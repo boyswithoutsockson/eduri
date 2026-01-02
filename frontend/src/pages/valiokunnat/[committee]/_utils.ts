bccfb622d632d490815dfa12d95b7499ccc4d3a1
keywell <25531990+ludovico-chiavelli@users.noreply.github.com>
2026-01-02 14:27:46 +0000

    feat: :sparkles: add a function to get a list of "master" MP objects so you don't have to keep doing queries

--- frontend/src/pages/valiokunnat/[committee]/_utils.ts
+++ frontend/src/pages/valiokunnat/[committee]/_utils.ts
@@ -7,3 +7,38 @@ export async function committeeData() {
         props: { committee },
     }));
 }
+
+export async function getMPsMasterObjectList() {
+    // Get basic MP data, party affiliation and committee memberships. Extend as needed..
+    const data = await db
+        .selectFrom("persons")
+        .leftJoinLateral(
+            (eb) =>
+                eb
+                    .selectFrom("mp_parliamentary_group_memberships as mppm")
+                    .select(["mppm.pg_id", "mppm.end_date"])
+                    .whereRef("mppm.person_id", "=", "persons.id")
+                    .orderBy("mppm.end_date", (ob) => ob.desc().nullsLast())
+                    .as("parliamentary_group"),
+            (join) => join.onTrue(),
+        )
+        .leftJoin("mp_committee_memberships", (join) =>
+            join.onRef("mp_committee_memberships.person_id", "=", "persons.id"),
+        )
+        .select([
+            "persons.id",
+            "persons.first_name",
+            "persons.last_name",
+            "persons.photo",
+            "persons.email",
+            "persons.occupation",
+            "persons.place_of_residence",
+            "parliamentary_group.pg_id as party_id",
+            "mp_committee_memberships.committee_name",
+            "mp_committee_memberships.start_date",
+            "mp_committee_memberships.end_date",
+            "mp_committee_memberships.role",
+        ])
+        .execute();
+    return data;
+}
