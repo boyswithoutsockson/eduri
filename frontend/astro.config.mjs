4ceb5c94635eb627813fdcb4ec5eab4fbfc1833e
Ludovico C. <key.well@outlook.com>
2026-01-31 14:01:18 +0000

    fix: :bug: add astro adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    build: {
-        concurrency: 4,
-    },
-    server: {
-        host: true,
-    },
+  build: {
+      concurrency: 4,
+  },
+
+  server: {
+      host: true,
+  },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
